bf260f518ee42de9b449922c72885b0b5611c1b5
Leonid Tsarev <leo@bastilia.ru>
2014-11-15 11:57:14 +0300

    add admin for geography

--- claims/admin.py
+++ claims/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from claims import models
 
 # Register your models here.
+admin.site.register(models.AddressCountry)
+admin.site.register(models.AddressRegion)
+admin.site.register(models.AddressCity)
