17e66b8972c7b8dcde3472d48131f111aeae03b5
Leonid Tsarev <leo@bastilia.ru>
2014-11-16 18:51:51 +0300

    Add statis project discovery page

--- claims/urls.py
+++ claims/urls.py
@@ -5,4 +5,7 @@ from claims import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^about/?$', views.about, name='about'),
+      
+    #project area
+    url(r'^project/discover/?$', views.project_discover, name='project_discover'),
 )
--- claims/views.py
+++ claims/views.py
@@ -6,3 +6,6 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def project_discover(request):
+    return render(request, 'projects/discover.html')
