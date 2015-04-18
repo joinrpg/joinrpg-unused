4b92bef280790e3bea3287ce5738d84244705e93
Leonid Tsarev <leo@bastilia.ru>
2015-04-18 13:56:28 +0300

    Use transaction.atomic when save project

--- claims/views.py
+++ claims/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import ModelForm
 from claims import models
+from django.db import transaction
 
 def index(request):
     return render(request, 'start.html')
@@ -18,22 +19,26 @@ class CreateProjectForm(ModelForm):
         model = models.Project
         fields = ['name', 'external_uri', 'description', 'game_begin_date', 'game_end_date']
 
+@transaction.atomic        
+def save_project_to_db(instance):
+    instance.author = models.User.objects.first() # TODO[Dair] Как сохранять здесь правильного пользователя?
+    instance.status = models.ProjectStatus.objects.filter(is_default_status=True).first()
+    instance.save() #Сохраняем
+    root_acl = models.ProjectAcl.create_author_fullcontrol(instance)
+    root_acl.save() #grant author fullcontrol rights
+    root_object = models.Object()
+    root_object.project = instance
+    root_object.name = 'Вся игра'
+    root_object.author = instance.author
+    root_object.save()
+    return instance.id
+    
 def project_create(request):
     if request.method == 'POST':
         form = CreateProjectForm(request.POST)
         if form.is_valid():
             instance = form.save (commit=False) # Не сохраняем пока в базу данных
-            instance.author = models.User.objects.first() # TODO[Dair] Как сохранять здесь правильного пользователя?
-            instance.status = models.ProjectStatus.objects.filter(is_default_status=True).first()
-            instance.save() #Сохраняем
-            root_acl = models.ProjectAcl.create_author_fullcontrol(instance)
-            root_acl.save() #grant author fullcontrol rights
-            root_object = models.Object()
-            root_object.project = instance
-            root_object.name = 'Вся игра'
-            root_object.author = instance.author
-            root_object.save()
-            return redirect('project_start', project_id = instance.id)
+            return redirect('project_start', project_id = save_project_to_db(instance))
     else:
         form = CreateProjectForm()
     return render(request, 'projects/create.html', {'form': form})
